09be934cfa31a3d24649a1368681ab067d5647b2
Bryan Finster <bryan.finster@gmail.com>
2025-10-19 21:02:29 -0500

    fix: upgrade tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	resolve: {
 		alias: {
 			$domain: path.resolve('./src/domain'),
